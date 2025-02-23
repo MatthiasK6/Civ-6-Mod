3aae814bdbf03972dd640a41787e6c40e60350e4
MatthiasK6 <matthias.kamann.lmg@gmail.com>
2025-02-23 20:17:53 +0100

    Add files via upload

---
+++ 2337169924/MD2.sql
@@ -0,0 +1,15 @@
+
+
+INSERT INTO Modifiers
+        (ModifierId,                                            ModifierType,								RunOnce,	Permanent)
+VALUES  ('MODIFIER_MORE_DISTRICT',			'MODIFIER_PLAYER_CITIES_EXTRA_DISTRICT',	0,			10);
+
+
+
+INSERT INTO ModifierArguments
+    	(ModifierId,                      						Name,	    	Value)
+VALUES 	('MODIFIER_MORE_DISTRICT', 			'Amount',   	10);
+
+
+
+
